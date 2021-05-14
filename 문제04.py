a7be695ccf1abe26f2b2160f2fc249c380e982f8
iruy <jeon1310>
2021-05-14 09:05:59 +0900

    문제4번 java 스타일의 swap

---
+++ 문제04.py
@@ -0,0 +1,21 @@
+def solution(arr):
+    left  = 0
+    right= len(arr) - 1
+    
+    #TODO : @@@ 빈칸 채우기
+    while @ @ @:
+        #arr[left], arr[right] = arr[right], arr[left]
+        temp = arr[left]
+        arr[left] = arr[right]
+        arr[right] = temp
+        left += 1
+        right -= 1
+    return arr
+
+
+# The following is code to output testcase.
+arr = [1, 4, 2, 3]  # input arr
+ret = solution(arr)
+
+# Press Run button to receive output.
+print("Solution: return value of the function is ", ret, " .")
