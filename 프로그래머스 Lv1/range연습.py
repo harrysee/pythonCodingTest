2a9e9533305d3bc05c6e206e9869c778f1a9f2b0
Jyuri <jeon1310@gmail.com>
2021-10-22 09:35:30 +0900

    기초 알고리즘 3번 - range출력

---
+++ 프로그래머스 Lv1/range연습.py
@@ -0,0 +1,7 @@
+# 3.range를 이용하여 1부터 7까지 출력하는 코드를 최대한 여러가지로 표현해보기
+# ex) N부터 M까지의 숫자를 출력하는 print_range(N, M) 함수를 정의하기
+def print_range(N, M):
+    for i in range(N,M+1):
+        print(i)
+
+print_range(1,7)
