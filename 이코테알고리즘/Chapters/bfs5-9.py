2b2f39130416ff1e76fccb6a1640328c6ceb1257
Jyuri <jeon1310@gmail.com>
2022-03-23 22:50:20 +0900

    BFS : 넓이우선탐색 실습

---
+++ 이코테알고리즘/Chapters/bfs5-9.py
@@ -0,0 +1,41 @@
+# BFS 넓이 우선 탐색 프로그램
+# 큐 이용
+# DFS보다 빠르게 작동함
+from collections import deque
+
+# BFS 메서드 정의
+def bfs(graph, start, visited):
+    # 큐(Queue) 구현을 위해 deque 라이브러리 사용
+    queue = deque([start])
+    # 현재 노드를 방문 처리
+    visited[start] = True
+    # 큐가 빌 때까지 반복
+    while queue:
+        # 큐에서 하나의 원소 뽑아 출력
+        v = queue.popleft()
+        print(v, end=' ')
+        # 해당 원소와 연결된, 아직 방문하지 않은 원소들을 큐에 삽입
+        for i in graph[v]:
+            if not visited[i]:
+                queue.append(i)
+                visited[i] =True
+
+# 인접 리스트 방식 : 1번인덱스(노드번호)에 연결된 노드인덱스 넣기
+# 각 노드가 연결된 정보를 리스트 자료형으로 표현(2차원 리스트)
+graph = [
+    [],
+    [2,3,8],
+    [1,7],
+    [1,4,5],
+    [3,5],
+    [3,4],
+    [7],
+    [2,6,8],
+    [1,7]
+]
+
+# 방문기록 리스트
+visited = [False]*9
+
+# 정의된 BFS 함수 호출
+bfs(graph,1,visited)
