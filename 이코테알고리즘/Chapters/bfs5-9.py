456f8407dd9080471680b091acd758b0507ad536
Jyuri <jeon1310@gmail.com>
2022-03-24 16:50:51 +0900

    DFS/BFS - 미로찾기

--- 이코테알고리즘/Chapters/bfs5-9.py
+++ 이코테알고리즘/Chapters/bfs5-9.py
@@ -6,7 +6,7 @@ from collections import deque
 # BFS 메서드 정의
 def bfs(graph, start, visited):
     # 큐(Queue) 구현을 위해 deque 라이브러리 사용
-    queue = deque([start])
+    queue = deque([start])  # 큐의 위치를 현재로 이동
     # 현재 노드를 방문 처리
     visited[start] = True
     # 큐가 빌 때까지 반복
