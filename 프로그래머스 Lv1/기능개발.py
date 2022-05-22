6b7ba54dea253756277552f5def89ad196b87552
Jyuri <jeon1310@gmail.com>
2022-05-22 23:06:25 +0900

    기능개발 : 프로그래머스 큐/스택 코드 추가

--- 프로그래머스 Lv1/기능개발.py
+++ 프로그래머스 Lv1/기능개발.py
@@ -70,3 +70,24 @@ def solution1(progresses, speeds):
 
 print(solution1([40, 93, 30, 55, 60, 65],[60, 1, 30, 5 , 10, 7]))
 print(solution1([93, 30, 55, 60, 40, 65],[1, 30, 5 , 10, 60, 7]))
+
+from math import ceil
+
+# 참고하여 공부한 코드
+def solution(progresses, speeds):
+    daysLeft = list(map(lambda x: (ceil((100 - progresses[x]) / speeds[x])), range(len(progresses))))
+    count = 1
+    retList = []
+
+    for i in range(len(daysLeft)):
+        try:
+            if daysLeft[i] < daysLeft[i + 1]:
+                retList.append(count)
+                count = 1
+            else:
+                daysLeft[i + 1] = daysLeft[i]
+                count += 1
+        except IndexError:
+            retList.append(count)
+
+    return retList
